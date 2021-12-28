217ba274c38af8974604698cdab337b890a57f6b
nguyendangbanghn93 <nguyendangbanghn@gmail.com>
2021-12-29 06:57:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="bgcf7 pf t0 l0 w1 z999k">
+        <div className="container df fww jcsb aic pa10">
+          <img
+            alt="ftech.ai"
+            src="https://ftech.ai/images/logo2.png"
+            width="198"
+            height="65"
+          />
+          <div>1</div>
+        </div>
+      </div>
+
+      <div
+        className="pb43 pb249-sm  bgrn bgpc bgsc banner-1 pr"
+        style={{ backgroundImage: "url('https://ftech.ai/images/anhbia.jpg')" }}
+      ></div>
+
+      <div className="container df fww mt50">
+        <div className="col-xs-12 col-md-6 plr10">
+          <div className="fs3 fwb">Kết quả</div>
+          <div className="fs2 lh15 mt15">
+            FTECH là một công ty công nghệ đầu tư nghiên cứu chuyên sâu về lĩnh
+            vực AI, trí tuệ nhân tạo, là một lĩnh vực của tương lai.
+          </div>
+        </div>
+        <div className="col-xs-12 col-md-6 plr10">
+          <div
+            className="pb169"
+            style={{
+              backgroundImage:
+                "url('https://cms.ftech.ai//uploads/14_feede9bf80.PNG')",
+            }}
+          ></div>
+        </div>
+      </div>
+
+      <div
+        className="pb51 bgrn bgpc bgsc banner-2 pr bt1 bss bcf mt50"
+        style={{ backgroundImage: "url('https://ftech.ai/images/anhbia.jpg')" }}
+      ></div>
     </div>
   );
 }
