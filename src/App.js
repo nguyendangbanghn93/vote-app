d821e006e74e45c64474702708a25b617e94c455
bangnd <nguyendangbanghn@gmail.com>
2021-12-31 11:22:41 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import Slider from "react-slick";
 import "./App.css";
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="">
       <div className="bgcf7 pf t0 l0 w1 z999k">
@@ -11,7 +40,7 @@ function App() {
             width="198"
             height="65"
           />
-          <div>1</div>
+          <div className="o0">1</div>
         </div>
       </div>
 
@@ -43,6 +72,56 @@ function App() {
         className="pb51 bgrn bgpc bgsc banner-2 pr bt1 bss bcf mt50"
         style={{ backgroundImage: "url('https://ftech.ai/images/anhbia.jpg')" }}
       ></div>
+
+      <div className="mtb50 ptb50 plr50">
+        <Slider {...settings}>
+          {[
+            "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+            "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+            "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+            "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          ].map((l) => (
+            <div className="plr10">
+              <div
+                className="pb169 bgrn bgpc bgsc"
+                style={{
+                  backgroundImage: `url('${l}')`,
+                }}
+              ></div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      <div className="df fdc jcsc pa10">
+        <div className="fs3 fwb tac">DANH SÁCH KHEN THƯỞNG</div>
+        <div className=" fs3 pr hrTitle tac">•</div>
+      </div>
+
+      <div className="df fww container ">
+        {[
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+          "http://cms.ftech.ai/uploads/tang8_3a7e6286a5.JPG",
+        ].map((l) => (
+          <div className="pa10 col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div
+              className="pb34 bgrn bgpc bgsc pr avt"
+              style={{
+                backgroundImage: `url('${l}')`,
+              }}
+              title="Vux vê lơ"
+            ></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
